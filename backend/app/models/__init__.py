dcd908f2dec7a737120ad9c43726d9ad04cac73e
Rohan Thawani <Thawaniroh000@gmail.com>
2026-07-09 19:39:17 -0700

    Aggregate all models in models/__init__.py for Alembic autodiscovery

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,8 @@
+from app.models.user import User
+from app.models.linked_identity import LinkedIdentity
+from app.models.account import Account
+from app.models.transaction import Transaction
+from app.models.category import Category
+from app.models.plan import Plan, PlanLineItem
+from app.models.subscription import Subscription
+from app.models.bill import Bill
